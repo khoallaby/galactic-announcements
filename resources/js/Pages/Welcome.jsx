667f85ced9d1b31461426445bf9f3f34f29a3304
Andy Nguyen <andrew.nguyen@colorado.edu>
2024-08-22 23:17:14 -0600

    Welcome - delete unused code

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,14 +1,6 @@
-import { Link, Head } from '@inertiajs/react';
 import Announcements from "@/Components/Announcements.jsx";
 
-export default function Welcome({ auth, laravelVersion, phpVersion }) {
-    const handleImageError = () => {
-        document.getElementById('screenshot-container')?.classList.add('!hidden');
-        document.getElementById('docs-card')?.classList.add('!row-span-1');
-        document.getElementById('docs-card-content')?.classList.add('!flex-row');
-        document.getElementById('background')?.classList.add('!hidden');
-    };
-
+export default function Welcome() {
     return (
         <>
             <div className="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
