e74cdbbdfaa168496a7104bbc45984fff4b10e25
Andy Nguyen <andrew.nguyen@colorado.edu>
2024-08-23 09:11:15 -0600

    api - working pagination logic

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+#use Illuminate\Support\Collection;
+#use Illuminate\Pagination\LengthAwarePaginator;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -16,6 +19,28 @@ Route::get('/announcements', function (Request $request) {
     $json = Storage::disk('local')->get('announcements.json');
     # parses json -> array
     $array = json_decode($json, true);
+
+
+    # working pagination logic
+    /*
+    $currentPage = $page ?? request('page', 1);
+    $perPage = 10;
+
+    # convert data to Collection to be able to paginate
+    $collection = new Collection($array);
+
+    $paginatedData = new LengthAwarePaginator(
+        $collection->forPage($currentPage, $perPage),
+        $collection->count(),
+        $perPage,
+        $currentPage,
+        ['path' => url('/announcements')]
+    );
+
+    return response()->json($paginatedData)->setEncodingOptions(JSON_NUMERIC_CHECK);
+    */
+
+
 
     return response()->json($array);
 });
