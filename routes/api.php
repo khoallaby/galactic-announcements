bb99df5519bf59acbb670de9e816e22d0d25b9f7
Andy Nguyen <andrew.nguyen@colorado.edu>
2024-08-22 21:35:31 -0600

    /api/announcements/ endpoint

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+/*
+ * api endpoint for getting all the announcements, from the json file
+ */
+Route::get('/announcements', function (Request $request) {
+    # gets json file from /storage/app/
+    $json = Storage::disk('local')->get('announcements.json');
+    # parses json -> array
+    $array = json_decode($json, true);
+
+    return response()->json($array);
+});
